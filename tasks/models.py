2956b65b83e23f0f54e048f683cabfea61651b1f
AlexsandroMO <sandrobass@htomail.com>
2020-10-21 20:11:17 -0300

    Atualization

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    inspection_name = models.CharField(max_length=255)
+    description = models.TextField()
+    link_path_pdf = models.TextField()
+    link_path_xlsx = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.inspection_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+ STATUS = (
+        (1, 'Doing'),
+        (2, 'Done')
+    )
+ done = models.CharField(
+        max_length=1,
+        choices=STATUS,
+    ) """
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . models import Task
+
 def hello(request):
     return HttpResponse('hello!')
 
 
 def index(request):
-    return render(request, 'tasks/index.html')
+    tasks = Task.objects.all()
+    
+    return render(request, 'tasks/index.html', {'tasks': tasks})
 
 def entryvalue(request, name):
+
     return render(request, 'tasks/entryvalue.html', {'name':name})
 
