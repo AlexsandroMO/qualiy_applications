d6a2dfc5a67eef537d11d103e0fde685d5c69893
AlexsandroMO <sandrobass@htomail.com>
2020-10-23 14:02:48 -0300

    update

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('hello/', views.hello),
     path('', views.index, name='index-list'),
+    path('task/<int:id>', views.taskview, name='task-view'),
+    path('newtask/', views.newtask, name='new-task'),
     path('entryvalue/<str:name>', views.entryvalue, name='entry-value'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import TaskForm
 
-from . models import Task
+from .models import Task
 
 def hello(request):
     return HttpResponse('hello!')
 
 
 def index(request):
-    tasks = Task.objects.all()
-    
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/index.html', {'tasks': tasks})
 
-def entryvalue(request, name):
 
+def entryvalue(request, name):
     return render(request, 'tasks/entryvalue.html', {'name':name})
+
+
+def taskview(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task':task})
+
+
+def newtask(request):
+    if request.method == 'POST':
+        form = TaskForm()
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('/')
+
+
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/add-task.html', {'form':form})
 
