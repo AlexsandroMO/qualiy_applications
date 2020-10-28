1e3347ea4b2cfda5b391269a1bfd456a15fa0617
AlexsandroMO <sandrobass@htomail.com>
2020-10-28 17:35:58 -0300

    Updates

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index-list'),
     path('task/<int:id>', views.taskview, name='task-view'),
     path('newtask/', views.newtask, name='new-task'),
+    path('edit/<int:id>', views.editview, name='edit-view'),
     path('entryvalue/<str:name>', views.entryvalue, name='entry-value'),
 ]
